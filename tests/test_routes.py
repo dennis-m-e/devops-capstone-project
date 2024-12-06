f9d9c98cc450d8b7e36161cd223501a57b43288b
Dennis Eder <dennis.eder2@de.bosch.com>
2024-12-06 04:46:57 -0500

    add read account functionality and test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -124,3 +124,22 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
+    def test_read_an_account(self):
+        """It should read an account"""
+        account = AccountFactory()
+        response = self.client.post(BASE_URL,
+            json=account.serialize(),
+            content_type="application/json"
+        )
+
+        id = response.get_json()["id"]
+        response = self.client.get(f"{BASE_URL}/{id}", content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_account_not_found(self):
+        """It should fail to read an account"""
+        response = self.client.get(f"{BASE_URL}/{0}", content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
